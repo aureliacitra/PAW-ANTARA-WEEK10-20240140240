8bcc8ac38e93a421d20a21b4a29b1c8c211c6992
aureliacitra <citrapangukir@gmail.com>
2026-08-17 15:25:36 +0700

    chat router bener

--- PAW-ANTARA-WEEK10-20240140240/routes/chat.routes.js
+++ PAW-ANTARA-WEEK10-20240140240/routes/chat.routes.js
@@ -4,6 +4,25 @@ const validateChatInput = require('../middlewares/validateChatInput.middleware')
 const { chat } = require('../controllers/chat.controller');
 const { saveMessage, getHistory } = require('../controllers/chatHistory.controller');
 
+/**
+ * BUG FIX: session express (express-session) defaultnya cuma ngirim cookie
+ * session kalo req.session "disentuh" (ada yang ditulis ke dalamnya) - karena
+ * saveUninitialized: false di app.js. Endpoint admin gak masalah karena login
+ * selalu nulis req.session.adminId. Tapi endpoint chat ini publik/anonim, gak
+ * pernah nulis apa-apa ke session, jadi cookie session gak pernah ke-set -
+ * akibatnya tiap request dianggap "session baru" & riwayat gak pernah ketemu
+ * lagi pas GET. Fix-nya: paksa "sentuh" session di awal biar cookie-nya jadi
+ * persist di browser.
+ */
+function ensureSession(req, res, next) {
+  if (!req.session.chatSessionStarted) {
+    req.session.chatSessionStarted = true;
+  }
+  next();
+}
+
+router.use(ensureSession);
+
 // endpoint public, user gak perlu login buat nanya ke CS bot
 router.post('/', validateChatInput, chat);
 
